0e190887bbb1a57019cffcbe3ca958a7b5808801
Bello Ango <angobello0@gmail.com>
2023-07-24 15:24:34 +0100

    debug to false

--- main/prod_settings.py
+++ main/prod_settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # NOTE: DONT FORGET TO REMOVE LOCALHOST FROM CSRF_TRUSTED_ORIGINS
 CSRF_TRUSTED_ORIGINS = [
